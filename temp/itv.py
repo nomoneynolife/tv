b7534732a3ee479545accde8cf3665f1cb19f15a
nomoneynolife <53741948+nomoneynolife@users.noreply.github.com>
2024-10-12 14:05:25 +0800

    Add files via upload

---
+++ temp/itv.py
@@ -0,0 +1,254 @@
+import time
+import os
+import concurrent.futures
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import re
+
+urls = [
+    #直辖市
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5YyX5LqsIg%3D%3D", # 北京市 77
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5aSp5rSlIg%3D%3D", # 天津市 8
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6YeN5bqGIg%3D%3D", # 重庆市 29
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5LiK5rW3Ig%3D%3D", # 上海市 43
+    #普通行政省
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki",                  # 河北省 246
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iU2hhbnhpIg%3D%3D",          # 山西省 728
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6L695a6B55yBIg%3D%3D",      # 辽宁省 28
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5ZCJ5p6X55yBIg%3D%3D",      # 吉林省 36
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6buR6b6Z5rGf55yBIg%3D%3D",  # 黑龙江省 6
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rGf6IuPIg%3D%3D",          # 江苏省 37
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rWZ5rGf55yBIg%3D%3D",      # 浙江省 201
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5a6J5b6955yBIg%3D%3D",      # 安徽省 181
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i56aP5bu655yBIg%3D%3D",      # 福建省 21
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rGf6KW%2FIg%3D%3D",        # 江西省 48
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5bGx5LicIg%3D%3D",          # 山东省 306
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",          # 河南省 515
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5YyXIg%3D%3D",          # 湖北省 57
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5Y2XIg%3D%3D",          # 湖南省 193
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iZ3Vhbmdkb25nIg%3D%3D",      # 广东省 253
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rW35Y2X55yBIg%3D%3D",      # 海南省 13
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5Zub5bed55yBIg%3D%3D",      # 四川省 31
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6LS15bee55yBIg%3D%3D",      # 贵州省 2
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5LqR5Y2X55yBIg%3D%3D",      # 云南省 23
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6ZmV6KW%2FIg%3D%3D",        # 陕西省 23
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i55SY6IKD55yBIg%3D%3D",      # 甘肃省 2
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6Z2S5rW355yBIg%3D%3D",      # 青海省 0
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5Y%2Bw5rm%2B55yBIg%3D%3D",  # 台湾省 0
+    #民族自治区
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5YaF6JKZ5Y%2BkIg%3D%3D",   #  内蒙古自治区 460
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5bm%2F6KW%2FIg%3D%3D"      #  广西壮族自治区 1662
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5a6B5aSPIg%3D%3D",        #  宁夏回族自治区 0
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5paw55aGIg%3D%3D",        #  新疆维吾尔自治区 0
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6KW%2F6JePIg%3D%3D",      #  西藏自治区 0
+    #特别行政区
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6aaZ5rivIg%3D%3D",  # 香港特别行政区 2
+    #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5r6z6ZeoIg%3D%3D",  # 澳门特别行政区 0
+    ]
+
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/iptv/live/1000.json?key=txiptv"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
+
+    return modified_urls
+
+
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=0.5)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
+
+
+results = []
+
+for url in urls:
+    try:
+        # 创建一个Chrome WebDriver实例
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+    
+        driver = webdriver.Chrome(options=chrome_options)
+        # 使用WebDriver访问网页
+        driver.get(url)  # 将网址替换为你要访问的网页地址
+        time.sleep(10)
+        # 获取网页内容
+        page_content = driver.page_source
+    
+        # 关闭WebDriver
+        driver.quit()
+    
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        x_urls = []
+        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+            url = url.strip()
+            ip_start_index = url.find("//") + 2
+            ip_end_index = url.find(":", ip_start_index)
+            ip_dot_start = url.find(".") + 1
+            ip_dot_second = url.find(".", ip_dot_start) + 1
+            ip_dot_three = url.find(".", ip_dot_second) + 1
+            base_url = url[:ip_start_index]  # http:// or https://
+            ip_address = url[ip_start_index:ip_dot_three]
+            port = url[ip_end_index:]
+            ip_end = "1"
+            modified_ip = f"{ip_address}{ip_end}"
+            x_url = f"{base_url}{modified_ip}{port}"
+            x_urls.append(x_url)
+        urls = set(x_urls)  # 去重得到唯一的URL列表
+    
+        valid_urls = []
+        #   多线程获取可用url
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            futures = []
+            for url in urls:
+                url = url.strip()
+                modified_urls = modify_urls(url)
+                for modified_url in modified_urls:
+                    futures.append(executor.submit(is_url_accessible, modified_url))
+    
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
+                if result:
+                    valid_urls.append(result)
+    
+        for url in valid_urls:
+            print(url)
+        # 遍历网址列表，获取JSON文件并解析
+        for url in valid_urls:
+            try:
+                # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+                ip_start_index = url.find("//") + 2
+                ip_dot_start = url.find(".") + 1
+                ip_index_second = url.find("/", ip_dot_start)
+                base_url = url[:ip_start_index]  # http:// or https://
+                ip_address = url[ip_start_index:ip_index_second]
+                url_x = f"{base_url}{ip_address}"
+    
+                json_url = f"{url}"
+                response = requests.get(json_url, timeout=0.5)
+                json_data = response.json()
+    
+                try:
+                    # 解析JSON文件，获取name和url字段
+                    for item in json_data['data']:
+                        if isinstance(item, dict):
+                            name = item.get('name')
+                            urlx = item.get('url')
+                            if ',' in urlx:
+                                urlx=f"aaaaaaaa"
+                                
+                            #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
+                            if 'http' in urlx:
+                                urld = f"{urlx}"
+                            else:
+                                urld = f"{url_x}{urlx}"
+    
+                            if name and urld:
+                                # 删除特定文字
+                                # 删除特定文字
+                                name = name.replace("cctv", "CCTV")
+                                name = name.replace("中央", "CCTV")
+                                name = name.replace("央视", "CCTV")
+                                name = name.replace("高清", "")
+                                name = name.replace("超高", "")
+                                name = name.replace("HD", "")
+                                name = name.replace("标清", "")
+                                name = name.replace("频道", "")
+                                name = name.replace("-", "")
+                                name = name.replace(" ", "")
+                                name = name.replace("PLUS", "+")
+                                name = name.replace("＋", "+")
+                                name = name.replace("(", "")
+                                name = name.replace(")", "")
+                                name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                                name = name.replace("CCTV1综合", "CCTV1")
+                                name = name.replace("CCTV2财经", "CCTV2")
+                                name = name.replace("CCTV3综艺", "CCTV3")
+                                name = name.replace("CCTV4国际", "CCTV4")
+                                name = name.replace("CCTV4中文国际", "CCTV4")
+                                name = name.replace("CCTV4欧洲", "CCTV4")
+                                name = name.replace("CCTV5体育", "CCTV5")
+                                name = name.replace("CCTV6电影", "CCTV6")
+                                name = name.replace("CCTV7军事", "CCTV7")
+                                name = name.replace("CCTV7军农", "CCTV7")
+                                name = name.replace("CCTV7农业", "CCTV7")
+                                name = name.replace("CCTV7国防", "CCTV7")
+                                name = name.replace("CCTV7国防军事", "CCTV7")
+                                name = name.replace("CCTV8电视剧", "CCTV8")
+                                name = name.replace("CCTV9记录", "CCTV9")
+                                name = name.replace("CCTV8纪录", "CCTV9")
+                                name = name.replace("CCTV9纪录", "CCTV9")
+                                name = name.replace("CCTV10科教", "CCTV10")
+                                name = name.replace("CCTV11戏曲", "CCTV11")
+                                name = name.replace("CCTV12社会与法", "CCTV12")
+                                name = name.replace("CCTV13新闻", "CCTV13")
+                                name = name.replace("CCTV新闻", "CCTV13")
+                                name = name.replace("CCTV14少儿", "CCTV14")
+                                name = name.replace("CCTV少儿", "CCTV14")
+                                name = name.replace("CCTV少儿超", "CCTV14")
+                                name = name.replace("CCTV15音乐", "CCTV15")
+                                name = name.replace("CCTV音乐", "CCTV15")
+                                name = name.replace("CCTV16奥林匹克", "CCTV16")
+                                name = name.replace("CCTV16奥运", "CCTV16")
+                                name = name.replace("CCTV17农业农村", "CCTV17")
+                                name = name.replace("CCTV17农业", "CCTV17")
+                                name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                                name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                                name = name.replace("CCTV5+体育", "CCTV5+")
+                                name = name.replace("CCTV教育", "CETV1")
+                                name = name.replace("风云足球", "CCTV风云足球")
+                                name = name.replace("上海卫视", "东方卫视")
+                                name = name.replace("内蒙卫视", "内蒙古卫视")
+                                name = name.replace("旅游卫视", "海南卫视")
+                                name = name.replace("天津卫视台", "天津卫视")
+                                name = name.replace("天津体育台", "天津体育")
+                                name = name.replace("天津公共台", "天津购物")
+                                name = name.replace("天津影视台", "天津影视")
+                                name = name.replace("天津文艺台", "天津文艺")
+                                name = name.replace("天津新闻台", "天津新闻")
+                                name = name.replace("天津科教台", "天津教育")
+                                name = name.replace("天津都市台", "天津都市")
+                                name = name.replace("CHC电影", "CHC高清电影")
+                                name = name.replace("江西少儿频", "江西少儿")
+                                name = name.replace("江西影视旅", "江西影视")
+                                name = name.replace("江西教育标", "江西教育")
+                                name = name.replace("教育电视台1", "CETV1")
+                                name = name.replace("CNB亚太财经", "CNBC亚洲频道")
+                                name = name.replace("东奥纪实", "BTV纪实科教")
+                                name = name.replace("法治天地", "法治天下")
+                                name = name.replace("淘电影", "BTV淘电影")
+                                name = name.replace("4K电源", "BTV4K超清")
+                                if 'udp' not in urld or 'rtp' not in urld:
+                                    results.append(f"{name},{urld}")
+                except:
+                    continue
+            except:
+                continue
+    except:
+        continue
+
+results = set(results)   # 去重得到唯一的URL列表
+results = sorted(results)
+with open("itv.txt", 'w', encoding='utf-8') as file:
+    for result in results:
+        file.write(result + "\n")
+        print(result)
